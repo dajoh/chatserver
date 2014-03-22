7073d9c99b77a61ecb68e5e1e8d65b857b4ecf87
Daniel Lindberg <dajoh10@gmail.com>
2014-03-22 13:59:50 +0100

    Cleaned up websocket code.

--- src/chatserver_ws.erl
+++ src/chatserver_ws.erl
@@ -7,6 +7,10 @@
 -export([websocket_info/3]).
 -export([websocket_terminate/3]).
 
+%% ------------------------------------------------------------------
+%% Websocket Callback Functions
+%% ------------------------------------------------------------------
+
 %% Called on HTTP request, upgrade to websocket protocol.
 init({tcp, http}, _Req, _Opts) ->
 	{upgrade, protocol, cowboy_websocket}.
@@ -17,101 +21,93 @@ websocket_init(_TransportName, Req, _Opts) ->
 
 %% Called when we get a JSON message.
 websocket_handle({text, Json}, Req, State) ->
-	try jiffy:decode(Json) of
-		{[{<<"type">>, <<"join">>}, {<<"name">>, Name}]} ->
-			if
-				byte_size(Name) < 16, byte_size(Name) > 0 ->
-					case chatserver_room:join_room(Name) of
-						{ok, ClientList} ->
-							{reply, {text, get_client_list_json(ClientList)}, Req, State};
-						{error, Reason} ->
-							{reply, {text, get_error_json(Reason)}, Req, State}
-					end;
-				true ->
-					{reply, {text, get_error_json(bad_name)}, Req, State}
-			end;
-		{[{<<"type">>, <<"message">>}, {<<"text">>, Text}]} ->
-			if
-				byte_size(Text) < 256, byte_size(Text) > 0 ->
-					chatserver_room:send_message(Text),
-					{ok, Req, State};
-				true ->
-					{reply, {text, get_error_json(bad_message)}, Req, State}
-			end;
-		{[{<<"type">>, <<"get_history">>}]} ->
-			{reply, {text, get_history_json()}, Req, State};
-
-		_ -> {shutdown, Req, State} %% unknown message
-	catch
-		_ -> {shutdown, Req, State} %% fuq the police
-	end;
+	handle_ejson_message(jiffy:decode(Json), Req, State);
 
 %% Called when we get an unknown message.
 websocket_handle(_Any, Req, State) ->
 	{ok, Req, State}.
 
-%% Called on broadcasts from other users.
-websocket_info({message, Name, Msg, Time}, Req, State) ->
-	Json = jiffy:encode({[
-		{type, message},
-		{from, Name},
-		{text, Msg},
-		{time, Time}
-	]}),
-	{reply, {text, Json}, Req, State};
-
-%% Called on connections from other users.
-websocket_info({connected, Name, Time}, Req, State) ->
-	Json = jiffy:encode({[
-		{type, user_joined},
-		{user, Name},
-		{time, Time}
-	]}),
-	{reply, {text, Json}, Req, State};
-
-%% Called on disconnects from other users.
-websocket_info({disconnected, Name, Time}, Req, State) ->
-	Json = jiffy:encode({[
-		{type, user_left},
-		{user, Name},
-		{time, Time}
-	]}),
-	{reply, {text, Json}, Req, State};
-
-%% Called on unknown system messages.
-websocket_info(_Info, Req, State) ->
-	{ok, Req, State}.
+%% Called on room messages.
+websocket_info(Msg, Req, State) ->
+	{reply, {text, jiffy:encode(get_room_message_ejson(Msg))}, Req, State}.
 
 %% Called when clients disconnect.
 websocket_terminate(_Reason, _Req, _State) ->
 	ok.
 
-%% Formats the client list as JSON.
-get_client_list_json(ClientList) ->
-	{_, Names} = lists:unzip(ClientList),
-	jiffy:encode({[
-		{type, user_list},
-		{list, Names}
-	]}).
+%% ------------------------------------------------------------------
+%% JSON Related Functions
+%% ------------------------------------------------------------------
+
+%% Handles join messages.
+handle_ejson_message({[{<<"type">>, <<"join">>}, {<<"name">>, Name}]}, Req, State) ->
+	if
+		byte_size(Name) < 16, byte_size(Name) > 0 ->
+			case chatserver_room:join_room(Name) of
+				{ok, ClientList} ->
+					Json = jiffy:encode(get_client_list_ejson(ClientList)),
+					{reply, {text, Json}, Req, State};
+				{error, Reason} ->
+					Json = jiffy:encode(get_error_ejson(Reason)),
+					{reply, {text, Json}, Req, State}
+			end;
+		true ->
+			Json = jiffy:encode(get_error_ejson(bad_name)),
+			{reply, {text, Json}, Req, State}
+	end;
+
+%% Handles user messages.
+handle_ejson_message({[{<<"type">>, <<"message">>}, {<<"text">>, Text}]}, Req, State) ->
+	if
+		byte_size(Text) < 256, byte_size(Text) > 0 ->
+			chatserver_room:send_message(Text),
+			{ok, Req, State};
+		true ->
+			Json = jiffy:encode(get_error_ejson(bad_message)),
+			{reply, {text, Json}, Req, State}
+	end;
+
+%% Handles history requests.
+handle_ejson_message({[{<<"type">>, <<"get_history">>}]}, Req, State) ->
+	Json = jiffy:encode(get_history_ejson(chatserver_hist:retrieve())),
+	{reply, {text, Json}, Req, State}.
+
+%% Formats messages as JSON.
+get_room_message_ejson({message, From, Text, Time}) -> {[
+		{type, message},
+		{from, From},
+		{text, Text},
+		{time, Time}
+	]};
+
+%% Formats connect messages as JSON.
+get_room_message_ejson({connected, User, Time}) -> {[
+		{type, user_joined},
+		{user, User},
+		{time, Time}
+	]};
+
+%% Formats disconnect messages as JSON.
+get_room_message_ejson({disconnected, User, Time}) -> {[
+		{type, user_left},
+		{user, User},
+		{time, Time}
+	]}.
 
 %% Formats errors as JSON.
-get_error_json(Reason) ->
-	jiffy:encode({[
+get_error_ejson(Reason) -> {[
 		{type, error},
 		{what, Reason}
-	]}).
+	]}.
 
 %% Formats the history buffer as JSON.
-get_history_json() ->
-	History = lists:map(
-		fun ({message, From, Text, Time}) ->
-				{[{type, message}, {from, From}, {text, Text}, {time, Time}]};
-			({connected, User, Time}) ->
-				{[{type, user_joined}, {user, User}, {time, Time}]};
-			({disconnected, User, Time}) ->
-				{[{type, user_left}, {user, User}, {time, Time}]}
-	end, chatserver_hist:retrieve()),
-	jiffy:encode({[
+get_history_ejson(History) -> {[
 		{type, history},
-		{hist, History}
-	]}).
+		{hist, lists:map(fun get_room_message_ejson/1, History)}
+	]}.
+
+%% Formats the client list as JSON.
+get_client_list_ejson(ClientList) -> {[
+		{type, user_list},
+		{list, element(2, lists:unzip(ClientList))}
+	]}.
